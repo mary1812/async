d81cc337acbbf163cc127aed78d866a402969962
Mary1812 <mariyapiligrim3@gmail.com>
2021-09-07 17:05:17 +0300

    Add json

--- js/index.js
+++ js/index.js
@@ -1,17 +1,32 @@
-let i = 0;
-const id = setInterval(function callback() {
-  if (i <= 20) {
-    console.log(i++)
-  }
-}, 100)
+// let i = 0;
+// const id = setInterval(function callback() {
+//   if (i <= 20) {
+//     console.log(i++)
+//   }
+// }, 100)
 
 
-let j = 0;
-let timerId = setTimeout(function timer() {
-  if (j<=20) {
-    console.log(j++);
-  }
-  timerId = setTimeout(timer, 100); 
-}, 100);
+// let j = 0;
+// let timerId = setTimeout(function timer() {
+//   if (j<=20) {
+//     console.log(j++);
+//   }
+//   timerId = setTimeout(timer, 100); 
+// }, 100);
 
+const obj = {
+  id: 1,
+  name: 'test',
+  lastname: 'test',
+  age: 100,
+  location: undefined,
+  test() {
+    
+  },
+  [Symbol()]: 'test'
+};
 
+const arr = [1, 2, 3]
+const jsonifiedObj = JSON.stringify(arr); //сериализация
+
+const rebuildedObj = JSON.parse(jsonifiedObj); //десериализация
