b335458e60e25a9b6dd6853cf56c365e152b2b7b
Mary1812 <mariyapiligrim3@gmail.com>
2021-09-09 17:03:02 +0300

    add some tasks

--- js/index.js
+++ js/index.js
@@ -1,15 +1,43 @@
-fetch("./js/data.json")
+"use strict";
+// fetch("./js/data.json")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log(data);
+//   });
+
+//   //Then принимает две функции , или catch в конце
+// fetch("./js/dddata.json")
+//   .then((response) => response.json(), (err) => {console.log("обработка ошибок в then")})
+//   .then((data) => {
+//     console.log(data);
+//   }).catch((err) => {
+//     console.log("error");
+//   });
+
+// const promise = new Promise((resolve, reject) => {
+// resolve('Hello from promise');
+//   reject(new Error('error'));
+// }).then(
+//   (data) => {
+//     console.log(data);
+//     return 1;
+//   },
+//   (error) => {
+//     console.error();(error);
+//   }
+// )
+
+const loadDiv = document.getElementById("load");
+
+const result = fetch("./js/data.json")
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
+    const jsonString = JSON.stringify(data, null, 4);
+    document.body.append(jsonString);
+  })
+  .catch((err) => {
+    document.body.append("ERROR HAPPEND");
+  })
+  .finally(() => {
+    loadDiv.remove();
   });
- 
-  //Then принимает два , или catch в конце
-fetch("./js/dddata.json")
-  .then((response) => response.json(), (err) => {console.log("обработка ошибок в then")})
-  .then((data) => {
-    console.log(data);
-  }).catch((err) => {
-    console.log("error");
-  });
-
